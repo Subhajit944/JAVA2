8b29f01cb9b2680f17fcd70095a9875ee43be86e
Subhajit Das <subhajit9004@gmail.com>
2026-05-04 22:18:27 +0530

    Add files via upload

---
+++ compareString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class compareString {
+public static void main(String[] args) {
+    String name1 ="tony";
+    String name2 ="stark";
+
+    if(name1.compareTo(name2) == 0){
+        System.out.println("String are equal");
+    }else{
+        System.out.println("String are not equal");
+    }
+
+    }
+    
+}
