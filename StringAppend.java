626a5e536d3771489af13fcf61dd69598eba6aea
Subhajit Das <subhajit9004@gmail.com>
2026-07-29 22:55:27 +0530

    Add files via upload

---
+++ StringAppend.java
@@ -0,0 +1,11 @@
+package Lacture13;
+public class StringAppend {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+}
